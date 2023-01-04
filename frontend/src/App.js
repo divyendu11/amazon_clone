dc5ebabba42ea33fb11f6c2e874334e8e9b4090a
Divyendu <gugusharma11@gmail.com>
2023-01-04 16:48:29 +0530

    Tut-3 How to use github

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href ="/">amazon clone</a>
+      </header>
+      <main> list products</main>
+    </div>
+  );
+}
+
+export default App;
