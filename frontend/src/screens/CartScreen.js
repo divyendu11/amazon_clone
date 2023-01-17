90d1c90dc2c77fab53ef25adc17f2558b759ee54
Divyendu <gugusharma11@gmail.com>
2023-01-17 18:34:11 +0530

    17) Complete Cart Screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -5,14 +5,39 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import MessageBox from "../components/MessageBox";
 import ListGroup from 'react-bootstrap/ListGroup';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/esm/Button";
+import axios from "axios";
+import Card from 'react-bootstrap/Card';
 export default function CartScreen() {
-
+    const navigate = useNavigate();
     const { state, dispatch: ctxDispatch } = useContext(Store);
     const {
         cart: { cartItems },
     } = state;
+
+
+
+    const updateCardHandler = async (item, quantity) => {
+        const { data } = await axios.get(`/api/products/${item._id}`);
+
+        if (data.countInStock < quantity) {
+            window.alert('Sorry, product is out of stock')
+
+            return;
+        }
+
+        ctxDispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantity } });
+    };
+
+    const removeItemHandler = (item) => {
+        ctxDispatch({ type: 'CART_REMOVE_ITEM', payload: { ...item, item } });
+    
+    };
+ 
+    const checkoutHandler = () => {
+        navigate('/signin?redirect=/shipping');
+    }
 
     return (
 
@@ -29,35 +54,57 @@ export default function CartScreen() {
                         {cartItems.map((item) => (
                             <ListGroup.Item key={item._id}>
                                 <Row className="align-items-center">
-                                <Col md={4}>
-                                    <img src={item.image} alt={item.name} className='img-fluid rounded img-thumbnail'></img>
-                                    {' '}
-                                    <Link to={`/product/${item.slug}`}>{item.name}</Link> 
-                                    {/* Linking it back to product screen */}
+                                    <Col md={4}>
+                                        <img src={item.image} alt={item.name} className='img-fluid rounded img-thumbnail'></img>
+                                        {' '}
+                                        <Link to={`/product/${item.slug}`}>{item.name}</Link>
+                                        {/* Linking it back to product screen */}
 
-                                </Col>
-                                <Col md={3}> 
-                                <Button variant='light' disabled={item.quantity === 1}>
-                                    <i className="fas fa-minus-circle"></i>
-                                </Button>{' '}
-                                <span>{item.quantity}</span>{' '}
-                                <Button variant='light' disabled={item.quantity === item.countInStock}>
-                                    <i className="fas fa-plus-circle"></i>
-                                </Button>
+                                    </Col>
+                                    <Col md={3}>
+                                        <Button variant='light' onClick={() => updateCardHandler(item, item.quantity - 1)} disabled={item.quantity === 1}>
+                                            <i className="fas fa-minus-circle"></i>
+                                        </Button>{' '}
+                                        <span>{item.quantity}</span>{' '}
+                                        <Button variant='light' onClick={() => updateCardHandler(item, item.quantity + 1)} disabled={item.quantity === item.countInStock}>
+                                            <i className="fas fa-plus-circle"></i>
+                                        </Button>
 
-                                </Col>
-                                <Col md={2}>${item.price}</Col>
-                                <Col md={2}>
-                                    <Button variant="light">
-                                        <i className="fas fa-trash"></i>
-                                    </Button>
-                                </Col>
+                                    </Col>
+                                    <Col md={2}>${item.price}</Col>
+                                    <Col md={2}>
+                                        <Button variant="light" onClick={() => removeItemHandler(item)}>
+                                            <i className="fas fa-trash"></i>
+                                        </Button>
+                                    </Col>
                                 </Row>
                             </ListGroup.Item>
                         ))}
                     </ListGroup>
                 )}</Col>
 
+
+
+                <Col md={4}>
+                    <Card>
+                        <Card.Body>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>
+                                    <h3>
+                                        Subtotal({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '}items):
+                                        ${cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
+                                    </h3>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <div className="d-grid">
+                                        <Button onClick={checkoutHandler} type="button" variant="primary" disabled={cartItems.length === 0}>Proceed To Checkout</Button>
+                                    </div>
+                                </ListGroup.Item>
+                            </ListGroup>
+
+                        </Card.Body>
+                    </Card>
+                </Col>
             </Row>
         </div>
 
